9c5bf3067a2e0c4c53200419ddaa6118128eed7c
Eduardo Apolonio dos Santos <112430322+SrDudu666@users.noreply.github.com>
2025-08-07 20:23:53 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,11 @@
+class Carro {
+     int velocidade;
+     String marca;
+     String modelo;
+     int idade;
+
+     int aumentarVelocidade(int velocidadeAdicional){
+
+     }
+
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        Carro monza = new Carro();
+        monza.velocidade = 0;
+        monza.marca = "chevrolet";
+        monza.idade = 2;
+
+        Carro fusca = new Carro();
+        fusca.velocidade = 0;
+        fusca.marca = "volkswagen";
+        fusca.idade = 7;
+
+    }
+}
